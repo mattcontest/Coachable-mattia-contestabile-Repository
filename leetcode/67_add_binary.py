821218ba7687aeaf7f752b37e439d161ae3bd47f
menth0s <marslite@pm.me>
2024-12-11 03:56:33 +0100

    Completed 67 Add Binary

---
+++ leetcode/67_add_binary.py
@@ -0,0 +1,30 @@
+#This problem gives us two binary numbers  in strings format
+# and our aim is to add them together.
+# We will take advantage of ord() to calcualte the int value of our chars
+# By doing so we can then add together the values, calculate the respective carry
+# and add it it before while we update our final solution.
+
+def addBinary(self, a: str, b: str) -> str:
+    #First initalizing my solution string
+    sol = "";
+    #Reverting the strings to ensure proper addition
+    #Time Complexity: O(n)
+    a,b = a[::-1], b[::-1];
+    #Initializing my carry to 0
+    carry = 0;
+    #Time Complexity O(n)
+    for i in range(max(len(a), len(b))):
+        digitA = ord(a[i]) - ord("0") if i < len(a) else 0;
+        digitB = ord(b[i]) - ord("0") if i < len(b) else 0;
+        total = digitA + digitB + carry;
+        char = str(total %2);
+        #O(1)
+        sol = char + sol;
+        carry = total // 2;
+        #O(n)
+        if(carry):
+            sol = str(carry) + sol;
+            
+        #Time Complexity O(n)
+        #Space Complexity O(n)
+    return sol;
