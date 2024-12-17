60c1e77cf5bc96a483b264323a0e3cd28d0b2b4f
menth0s <marslite@pm.me>
2024-12-17 03:28:10 +0100

    Addressing pylint issues for 811 and 680

--- leetcode/811_subdomain_visit_count.py
+++ leetcode/811_subdomain_visit_count.py
@@ -1,17 +1,19 @@
-
-#Loom explanation: https://www.loom.com/share/907a5311a24b45aa91c24296188ae424
+"""811. Subdomain Visit Count"""
 class Solution:
-    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+    """Solution Class"""
+    def subdomainVisits(self, cpdomains: list[str]) -> list[str]:
+        """
+        #Loom explanation: https://www.loom.com/share/907a5311a24b45aa91c24296188ae424
+        """
         #To first create an hashmap where I will be tracking
         #for each domain/subdomain how many times gets visited.
         counts = {}
-        #We will be iterating over the cpdoamins and start assigning the respective 
+        #We will be iterating over the cpdoamins and start
+        #assigning the respective
         #values to its respective domain.
-
         #Time Complexity O(k)
         for domain in cpdomains:
             count,domain = domain.split(" ")
-
             count = int(count)
             splittedDomains = domain.split(".")
             #Iterating over the splitted domains ~ subdomains
@@ -25,11 +27,10 @@ class Solution:
                     counts[subdomain] += count
                 else:
                     counts[subdomain] = count
-        
         #Time Complexity O(k) * O(m) * O(m) = O(k * m^2)
         #Space Complexity O(m)
-
         return [f"{count} {domain}" for domain, count in counts.items()]
+
 
 
 
