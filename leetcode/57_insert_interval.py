a5994e3dc5cb1ace623b1afc10ff32c065028b73
menth0s <marslite@pm.me>
2024-12-18 01:12:25 +0100

    57. Insert Interval

---
+++ leetcode/57_insert_interval.py
@@ -0,0 +1,22 @@
+#Loom: https://www.loom.com/share/49b3a7e8faf14012b274297443082570?sid=febff73b-da83-4333-b3e6-32789ee123a3
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #We will start by declaring an empty array that we will use to store the result
+        result = []
+        #Time Complexity: O(n)
+        #Space Complexity: O(n)
+        #We want to iterate over the range of intervals and check whether 
+        #First I'll be checkign that newInterval[1] is smaller than the start of my first interval
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            #We also have to merge the overlapping interval/newInterval
+            else:
+                newInterval = (min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1]))
+        result.append(newInterval)
+        return result
+
